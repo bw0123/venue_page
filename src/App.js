962ca369162b019bf77c6ec7534a13e1664af01d
Britney <72894015+bw0123@users.noreply.github.com>
2021-01-21 17:02:21 -0500

    App.js fix

--- src/App.js
+++ src/App.js
@@ -7,49 +7,9 @@ import Featured from './components/featured';
 import VenueNfo from './components/venueNfo';
 import Highlight from './components/Highlights';
 import Pricing from './components/pricing';
-import Bios from './components/artistBios/Bios';
 import Location from './components/location';
-import Faq from './components/featured/Faq';
+import Faq from './components/faq/Faq';
 import Footer from './components/header_footer/Footer';
-<<<<<<< HEAD
-
-class App extends Component {
-  render() {
-    return(
-      <div className="App" style={{height:"1500px", background:'cornflowerblue'}}>
-        <Header/>
-
-        <Element name="featured">
-          <Featured/>
-        </Element>
-
-        <Element name="venuenfo">
-          <VunueNfo/>
-        </Element>
-
-        <Element name="highlights">
-          <Highlight/>
-        </Element>
-
-        <Element name="pricing">
-          <Pricing/>
-        </Element>
-
-        <Element name="artistBios">
-          <Bios/>
-        </Element>
-
-        <Element name="location">
-          <Location/>
-        </Element>
-
-       <Element name="Faq">
-          <Faq/>
-        </Element>
-
-        <Footer/>
-=======
-import Faq from './components/featured/Faq';
 import Bios from './components/artistBios/Bios';
 
 class App extends Component {
@@ -77,22 +37,17 @@ class App extends Component {
       <Element name="artistBios">
       <Bios/>
       </Element>
-      
-      <Element name="faq">
-      <Faq/>
-      </Element>
 
       <Element name="location">
       <Location/>
       </Element>
 
-      <Element name="Faq">
+      <Element name="faq">
       <Faq/>
       </Element>
 
       <Footer/>
       
->>>>>>> 1d318f019fbf05dcfeae9c3eafbfd49fdd57250e
       </div>
     );
   }
